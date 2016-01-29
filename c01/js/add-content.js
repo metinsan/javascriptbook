29b1ab4f7afdfa65548a7d9690fc8d845940c96a
metinsan <metinemail@gmail.com>
2016-01-30 00:42:04 +0200

    index added

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -1,3 +1,20 @@
 /**
  * Created by PC on 28.1.2016.
  */
+
+/*jslint browser:true */
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = "Good evening!";
+} else if (hourNow > 12) {
+    greeting = "Good afternoon!";
+} else if (hourNow > 0) {
+    greeting = "Good morning!";
+} else {
+    greeting = "Welcome!";
+}
+
+document.write('<h3>' + greeting + '<h3>');
